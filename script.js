e52c2da71e16d352084c83751b3987083d6eb803
Matheus Soares <106754355+theussoares@users.noreply.github.com>
2024-11-09 22:46:55 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//Declare quatro variáveis;
+//nome, vai receber um valor do tipo string
+//sobrenome, vai receber um valor do tipo string
+//anoNascimento, vai receber um valor do tipo number
+//cidade, vai receber um valor do tipo string
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${anoAtual-anoNascimento} anos e moro em ${cidade}`)
